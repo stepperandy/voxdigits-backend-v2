9f469ff51bce56e8ee17b1f3c383693e2f0e2732
stepperandy <85257812+stepperandy@users.noreply.github.com>
2026-04-14 19:31:12 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-app.get("/", (req, res) => {
+app.get("/", function (req, res) {
   res.status(200).json({
     ok: true,
-    message: "VERSION 999 LIVE"
+    message: "VERSION 1000 LIVE"
   });
 });
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(VERSION 999 LIVE on port ${PORT});
+app.listen(PORT, "0.0.0.0", function () {
+  console.log("VERSION 1000 LIVE on port " + PORT);
 });
